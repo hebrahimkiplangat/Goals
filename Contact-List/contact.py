d4fe92d7a54f7fa4f91ecd112d020d7c1fa0bee9
ngenohzephenia <ngenohzef@gmail.com>
2021-03-21 08:47:23 +0300

    initial commit

---
+++ Contact-List/contact.py
@@ -0,0 +1,9 @@
+class Contact:
+    """
+    Class that generates new instances of contacts
+    """
+    def __init__(self,first_name,last_name,phone_number,email):
+     
+    __init
+    
+    pass
---
+++ breakthrough/breakthrough.py
@@ -0,0 +1,8 @@
+# syntax
+# function name(){}
+# def name():
+# pass
+
+def get_name():
+    print("Kizito")
+get_name()
